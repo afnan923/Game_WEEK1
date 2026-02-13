7acec281208140ac5d1f0f29a67f558f44242c5f
afnan923 <susumurni013@gmail.com>
2026-02-14 00:11:04 +0700

    add toogle music and sound

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -110,4 +110,19 @@ class AudioManager {
   void setSfxVolume(double volume) {
     _sfxVolume = volume.clamp(0.0, 1.0);
   }
+
+  /// Toggle music on/off
+  void toggleMusic() {
+    _isMusicEnabled = !_isMusicEnabled;
+    if (_isMusicEnabled) {
+      resumeBackgroundMusic();
+    } else {
+      pauseBackgroundMusic();
+    }
+  }
+
+  /// Toggle sound effects on/off
+  void toggleSfx() {
+    _isSfxEnabled = !_isSfxEnabled;
+  }
 }
