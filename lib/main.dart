1108c92ecc1d7c1256557d4e9337dd9181998696
afnan923 <susumurni013@gmail.com>
2026-02-13 11:35:40 +0700

    add text

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,10 @@ class _GameScreenState extends State<GameScreen> {
                 color: Colors.black54,
                 borderRadius: BorderRadius.circular(10),
               ),
-              
+              child: const Text(
+                'Score: 0',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
             ),
           ),
         ],
