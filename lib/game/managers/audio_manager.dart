801752c13eb5f5bc972a6a15738e409bc4e0601e
afnan923 <susumurni013@gmail.com>
2026-02-14 00:02:41 +0700

    add file audio_manager and sigleton pattern

---
+++ lib/game/managers/audio_manager.dart
@@ -0,0 +1,12 @@
+class AudioManager {
+    // Singleton pattern
+    static final AudioManager _instance = AudioManager._internal();
+    factory AudioManager() => _instance;
+    AudioManager._internal();
+
+    bool _isMusicEnabled = true;
+    bool _isSfxEnabled = true;
+    double _musicVolume = 0.7;
+    double _sfxVolume = 1.0;
+    
+}
