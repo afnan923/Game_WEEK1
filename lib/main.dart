3f54a6b0580e80290b8badcbbaed1bd0d916c94f
afnan923 <susumurni013@gmail.com>
2026-02-13 11:41:18 +0700

    add icon music note and volume up

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,22 @@ class _GameScreenState extends State<GameScreen> {
               ),
             ),
           ),
+          Positioned(
+            top: 50,
+            right: 20,
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.music_note, color: Colors.black),
+                   onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.volume_up, color: Colors.black),
+                   onPressed: () {},
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
